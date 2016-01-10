0b6a892bd8a54596fa773b061a228918a95f0e16
drakgoku1 <delive_cpl@hotmail.com>
2016-01-10 02:14:20 +0100

    Created by Coding Ground Client

---
+++ New Project/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+   cout << "\n Hello World - > Iostream \n" << endl;  // iostream
+   printf("\n Hello World - > Stdio \n\n   "); // stdio
+   system("PAUSE");
+   
+}
+
